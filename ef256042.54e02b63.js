b5726e55aaf73bd9647f90a1f91bfccfc345be62
orcadefi <dev@orcadefi.com>
2020-10-12 23:33:27 -0500

    Deploy website - based on

---
+++ ef256042.54e02b63.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{77:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"orca","name":"orca","count":1,"permalink":"/blog/tags/orca"}')}}]);
